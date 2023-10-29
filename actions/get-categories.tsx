0737c85e8a011af76a5c866aca0236b0a00e68ef
Atul-me <atulkur1999@gmail.com>
2023-10-29 16:30:52 +0530

    comm

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -5,7 +5,7 @@ const URL=`${process.env.NEXT_PUBLIC_API_URL}/categories`;
 const getCategories = async (): Promise<Category[]> => {
   const res = await fetch(URL);
   
-  console.log(res.json(),"CATS")
+  // console.log(res.json(),"CATS")
   return res.json();
 };
 
